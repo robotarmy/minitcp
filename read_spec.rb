954141becb883139ea61fdf621e0ce5e97ab29a0
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2011-03-21 18:49:43 -0700

    read_spec - updated to continue into instance variable

--- read_spec.rb
+++ read_spec.rb
@@ -1,19 +1,18 @@
 require File.join(File.dirname(__FILE__),'read')
 
 describe Read do
-
+  
   describe "#new" do
-
     it "has a constructor that takes a hash to set hostname" do
       read = Read.new({:hostname => 'hi.com'})
       read.hostname.should == 'hi.com'
-      r.valid?.should be_false
+      read.valid?.should be_false
     end
 
     it "has a constructor that takes a hash to set port" do
       read = Read.new({:port => '8990'})
-      read.hostname.should == '8990'
-      r.valid?.should be_false
+      read.port.should == '8990'
+      read.valid?.should be_false
     end
 
     it "is valid with a port and hostname " do
